9a38504d2f412bb8b6aa6b67fe06cc6421963f5a
ap1495 <ap14state@gmail.com>
2020-03-20 21:56:30 -0400

    Updates

---
+++ precache-manifest.651a7b660639e00f7acc218a1d5ab19f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7841fb8fb7505b9595c3329eb6412233",
+    "url": "/react-portfolio-app/index.html"
+  },
+  {
+    "revision": "9b1be69589a53a1b4d7e",
+    "url": "/react-portfolio-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "38fe227d3d1f83dcdb92",
+    "url": "/react-portfolio-app/static/css/main.48020f97.chunk.css"
+  },
+  {
+    "revision": "9b1be69589a53a1b4d7e",
+    "url": "/react-portfolio-app/static/js/2.df46dab7.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-portfolio-app/static/js/2.df46dab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fe227d3d1f83dcdb92",
+    "url": "/react-portfolio-app/static/js/main.87b7f746.chunk.js"
+  },
+  {
+    "revision": "1b59824a10f199f85d9a",
+    "url": "/react-portfolio-app/static/js/runtime-main.d3b6ca8a.js"
+  },
+  {
+    "revision": "5c6ca2aad664851e1365d293728f7d34",
+    "url": "/react-portfolio-app/static/media/abhishek.5c6ca2aa.png"
+  },
+  {
+    "revision": "fa79eecd83ddeed54f73bc2565ce8f8e",
+    "url": "/react-portfolio-app/static/media/linkedin.fa79eecd.jfif"
+  },
+  {
+    "revision": "5a3bd4e1ea954c5595dadfe38adb7588",
+    "url": "/react-portfolio-app/static/media/tableau.5a3bd4e1.png"
+  }
+]);
